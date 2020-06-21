e7953d1141baf66b29a3d468b8be60503fbc5d49
Andrzej Dybowski <dybowski@andrzejd.pl>
2020-06-21 17:56:42 +0200

    feat: add saver test

--- types_test.go
+++ types_test.go
@@ -29,15 +29,21 @@ func Test_jsonConfiguration_Load(t *testing.T) {
 	}{
 		{
 			name:    "empty",
-			fields:  fields{scheme: &jsonConfiguration{}},
+			fields:  fields{scheme: &testJsonConfig{}},
 			args:    args{json: "{}"},
 			wantErr: false,
 		},
 		{
 			name:    "good",
-			fields:  fields{scheme: &jsonConfiguration{}},
+			fields:  fields{scheme: &testJsonConfig{}},
 			args:    args{json: "{\"name\":\"Ok\",\"type\":0,\"comments\":[\"Test\"],\"real\":true,\"tester\":null}"},
 			wantErr: false,
+		},
+		{
+			name:    "good",
+			fields:  fields{scheme: &testJsonConfig{}},
+			args:    args{json: "{\"name\":[],\"type\":0,\"comments\":[\"Test\"],\"real\":true,\"tester\":null}"},
+			wantErr: true,
 		},
 	}
 	for _, tt := range tests {
@@ -48,6 +54,70 @@ func Test_jsonConfiguration_Load(t *testing.T) {
 			reader := strings.NewReader(tt.args.json)
 			if err := j.Load(reader); (err != nil) != tt.wantErr {
 				t.Errorf("Load() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func Test_jsonConfiguration_Save(t *testing.T) {
+	type fields struct {
+		scheme interface{}
+	}
+	tests := []struct {
+		name       string
+		fields     fields
+		wantWriter string
+		wantErr    bool
+	}{
+		{
+			name: "empty",
+			fields: fields{
+				scheme: testJsonConfig{
+					Name:     "",
+					Type:     0,
+					Comments: []string{},
+					Real:     false,
+					Tester:   nil,
+				},
+			},
+			wantWriter: "{\"name\":\"\",\"type\":0,\"comments\":[],\"real\":false,\"tester\":null}",
+			wantErr:    false,
+		},
+		{
+			name: "good",
+			fields: fields{
+				scheme: testJsonConfig{
+					Name:     "Test",
+					Type:     3,
+					Comments: []string{"www"},
+					Real:     true,
+					Tester: testJsonConfig{
+						Name:     "",
+						Type:     0,
+						Comments: []string{},
+						Real:     false,
+						Tester:   nil,
+					},
+				},
+			},
+			wantWriter: "{\"name\":\"Test\",\"type\":3,\"comments\":[\"www\"],\"real\":true," +
+				"\"tester\":{\"name\":\"\",\"type\":0,\"comments\":[],\"real\":false,\"tester\":null}}",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			j := &jsonConfiguration{
+				scheme: tt.fields.scheme,
+			}
+			writer := &bytes.Buffer{}
+			err := j.Save(writer)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Save() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if gotWriter := writer.String(); gotWriter != tt.wantWriter {
+				t.Errorf("Save() gotWriter = %v, want %v", gotWriter, tt.wantWriter)
 			}
 		})
 	}
